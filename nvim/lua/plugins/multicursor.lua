8cf416957125a4f31a436352b0e5cec27e111e4d
Jeremy Evans <jeremy@chiefgeek.io>
2025-11-10 08:57:00 -0500

    updating my keymaps for me

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -1,61 +1,45 @@
 return {
-    "jake-stewart/multicursor.nvim",
-    branch = "1.0",
-    config = function()
-        local mc = require("multicursor-nvim")
-        mc.setup()
+  "jake-stewart/multicursor.nvim",
+  branch = "1.0",
+  config = function()
+    local mc = require("multicursor-nvim")
 
-        local set = vim.keymap.set
+    mc.setup()
 
-        -- Add or skip cursor above/below the main cursor.
-        set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end)
-        set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end)
-        set({"n", "x"}, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-        set({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end)
+    -- Add cursors above/below the main cursor
+    vim.keymap.set({"n", "v"}, "<up>", function() mc.lineAddCursor(-1) end)
+    vim.keymap.set({"n", "v"}, "<down>", function() mc.lineAddCursor(1) end)
 
-        -- Add or skip adding a new cursor by matching word/selection
-        set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end)
-        set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end)
-        set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
-        set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
+    -- Add a cursor and jump to the next word under cursor
+    vim.keymap.set({"n", "v"}, "<c-n>", function() mc.matchAddCursor(1) end)
 
-        -- Add and remove cursors with control + left click.
-        set("n", "<c-leftmouse>", mc.handleMouse)
-        set("n", "<c-leftdrag>", mc.handleMouseDrag)
-        set("n", "<c-leftrelease>", mc.handleMouseRelease)
+    -- Jump to the next word under cursor but do not add a cursor
+    vim.keymap.set({"n", "v"}, "<c-s>", function() mc.matchSkipCursor(1) end)
 
-        -- Disable and enable cursors.
-        set({"n", "x"}, "<c-q>", mc.toggleCursor)
+    -- Delete the main cursor
+    vim.keymap.set({"n", "v"}, "<leader>x", mc.deleteCursor)
 
-        -- Mappings defined in a keymap layer only apply when there are
-        -- multiple cursors. This lets you have overlapping mappings.
-        mc.addKeymapLayer(function(layerSet)
+    -- Add and remove cursors with control + left/right click
+    vim.keymap.set("n", "<c-leftmouse>", mc.handleMouse)
 
-            -- Select a different cursor as the main one.
-            layerSet({"n", "x"}, "<left>", mc.prevCursor)
-            layerSet({"n", "x"}, "<right>", mc.nextCursor)
+    vim.keymap.set({"n", "v"}, "<c-q>", function()
+      if mc.cursorsEnabled() then
+        mc.clearCursors()
+      else
+        mc.restoreCursors()
+      end
+    end)
 
-            -- Delete the main cursor.
-            layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
+    -- Align cursor columns
+    vim.keymap.set("v", "<leader>a", mc.alignCursors)
 
-            -- Enable and clear cursors using escape.
-            layerSet("n", "<esc>", function()
-                if not mc.cursorsEnabled() then
-                    mc.enableCursors()
-                else
-                    mc.clearCursors()
-                end
-            end)
-        end)
-
-        -- Customize how cursors look.
-        local hl = vim.api.nvim_set_hl
-        hl(0, "MultiCursorCursor", { reverse = true })
-        hl(0, "MultiCursorVisual", { link = "Visual" })
-        hl(0, "MultiCursorSign", { link = "SignColumn"})
-        hl(0, "MultiCursorMatchPreview", { link = "Search" })
-        hl(0, "MultiCursorDisabledCursor", { reverse = true })
-        hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-        hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
-    end
+    -- Escape clears cursors
+    vim.keymap.set("n", "<esc>", function()
+      if not mc.cursorsEnabled() then
+        mc.enableCursors()
+      elseif mc.hasCursors() then
+        mc.clearCursors()
+      end
+    end)
+  end,
 }
