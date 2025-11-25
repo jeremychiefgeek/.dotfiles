1c6fd5b1862fa894889b622586474675cbf318f8
Jeremy Evans <jeremy@chiefgeek.io>
2025-11-24 20:51:59 -0500

    updating

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -22,6 +22,7 @@ return {
       lua = { "stylua" },
       python = { "isort", "black" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
       cpp = { "clang-format" },
       c = { "clang-format" },
     },
