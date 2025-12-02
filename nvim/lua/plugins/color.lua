1bcd628bf65b06a2c8b1dc4d7603f4fca950268d
Jeremy Evans <jeremy@chiefgeek.io>
2025-12-02 09:30:28 -0500

    Update to colorscheme

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,4 +1,11 @@
 return {
+  {
+    "https://github.com/jeremychiefgeek/compline.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme compline]])
+    end,
+  },
   -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
   -- {
   --   "bjarneo/ash.nvim",
@@ -7,11 +14,11 @@ return {
   --     vim.cmd([[colorscheme ash]])
   --   end,
   -- },
-  {
-    "blazkowolf/gruber-darker.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme gruber-darker]])
-    end,
-  },
+  -- {
+  --   "blazkowolf/gruber-darker.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd([[colorscheme gruber-darker]])
+  --   end,
+  -- },
 }
