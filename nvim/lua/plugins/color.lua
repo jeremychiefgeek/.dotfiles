ba3e6b2081c676ede456f3ac5dec31039b63f90a
Jeremy Evans <jeremy@chiefgeek.io>
2025-12-16 21:29:14 -0500

    switching to rose-pinr

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,11 +1,18 @@
 return {
   {
-    "https://github.com/jeremychiefgeek/compline.nvim",
-    priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-      vim.cmd([[colorscheme compline]])
+      vim.cmd("colorscheme rose-pine")
     end,
   },
+  -- {
+  --   "https://github.com/jeremychiefgeek/compline.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd([[colorscheme compline]])
+  --   end,
+  -- },
   -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
   -- {
   --   "bjarneo/ash.nvim",
