0ddd2893b08e0be25af1a201608eb29d357922ed
Jeremy Evans <jeremy@chiefgeek.io>
2025-12-29 18:26:43 -0500

    updating

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo pacman -S unzip pacman-contrib rofi jq fastfetch btop unzip
+sudo pacman -S unzip pacman-contrib rofi jq fastfetch btop unzip slurp grim swappy imagemagick wl-clipboard
 
