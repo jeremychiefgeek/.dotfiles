d16b2a6da6ac68cf6afe08a5a551743f81b5d43b
Jeremy Evans <jeremy@chiefgeek.io>
2026-01-12 13:11:42 -0500

    updating

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,14 +2,18 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons",
     },
+    init = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+    end,
     opts = function()
-      -- Lock root to current working directory.
-      -- If you want “git root”, I can swap this for a git-root resolver.
+      -- Lock to project root (cwd)
       local locked_root = vim.fn.fnamemodify(vim.fn.getcwd(), ":p")
 
       local function is_locked_root(path)
@@ -18,30 +22,28 @@ return {
 
       return {
         filesystem = {
-          hijack_netrw_behavior = "open_current",
           bind_to_cwd = true,
+          hijack_netrw_behavior = "open_default", -- still hijack, but use sidebar
 
           window = {
+            position = "left",
+            width = 32,
             mappings = {
-              -- Oil-ish
               ["-"] = "navigate_up_locked",
               ["<bs>"] = "navigate_up_locked",
               ["q"] = "close_window",
 
-              -- Prevent changing the root / jumping above it
-              ["C"] = "none", -- (neo-tree default: set_root)
-              ["."] = "none", -- (neo-tree default: set_root / set_root_to_node in some setups)
+              -- Block root changing
+              ["C"] = "none",
+              ["."] = "none",
             },
           },
 
           commands = {
             navigate_up_locked = function(state)
-              -- state.path is the directory currently being shown as the filesystem root
               if is_locked_root(state.path) then
-                return -- already at locked root; do nothing
+                return
               end
-
-              -- call neo-tree's built-in navigate_up
               require("neo-tree.sources.filesystem.commands").navigate_up(state)
             end,
           },
@@ -51,11 +53,15 @@ return {
     config = function(_, opts)
       require("neo-tree").setup(opts)
 
-      -- Open neo-tree in the current window like Oil
+      -- Toggle sidebar (Explorer)
+      vim.keymap.set("n", "<leader>e", function()
+        vim.cmd("Neotree filesystem reveal left")
+      end, { desc = "Explorer (Neo-tree)" })
+
+      -- Oil-style open but in sidebar
       vim.keymap.set("n", "-", function()
-        -- Ensure the tree roots at the locked root (cwd) and reveals current file
-        vim.cmd("Neotree position=current reveal")
-      end, { desc = "Explorer (neo-tree, locked root)" })
+        vim.cmd("Neotree filesystem reveal left")
+      end, { desc = "Explorer (Neo-tree)" })
     end,
   },
 }
