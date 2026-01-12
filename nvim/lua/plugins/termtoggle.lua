0d8a81f0a60cab14fda475d3b968359e8297df34
Jeremy Evans <jeremy@chiefgeek.io>
2026-01-12 13:08:41 -0500

    updating

--- nvim/lua/plugins/termtoggle.lua
+++ nvim/lua/plugins/termtoggle.lua
@@ -1,4 +1,24 @@
 return {
-  -- amongst your other plugins
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<leader>t]],
+      direction = "float",
+      start_in_insert = true,
+      close_on_exit = true,
+      shade_terminals = false,
+
+      float_opts = {
+        border = "rounded",
+        winblend = 0,
+        width = function()
+          return math.floor(vim.o.columns * 0.9)
+        end,
+        height = function()
+          return math.floor(vim.o.lines * 0.85)
+        end,
+      },
+    })
+  end,
 }
