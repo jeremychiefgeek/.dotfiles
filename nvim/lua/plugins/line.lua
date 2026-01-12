4eb93305318557feb67906d8541dc52412c5a344
Jeremy Evans <jeremy@chiefgeek.io>
2026-01-12 11:08:38 -0500

    updating

--- nvim/lua/plugins/line.lua
+++ nvim/lua/plugins/line.lua
@@ -1,32 +1,32 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-      require('lualine').setup {
-        options = {
-          theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch'},
-          lualine_c = {
-            'filename',
-            {
-              function()
-                if vim.bo.filetype == 'markdown' then
-                  local word_count = vim.fn.wordcount().words
-                  return 'word count: ' .. word_count
-                end
-                return ''
-              end,
-            }
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          "filename",
+          {
+            function()
+              if vim.bo.filetype == "markdown" then
+                local word_count = vim.fn.wordcount().words
+                return "word count: " .. word_count
+              end
+              return ""
+            end,
           },
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
         },
-      }
-    end
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
