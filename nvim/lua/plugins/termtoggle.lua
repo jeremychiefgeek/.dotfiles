a7e1322afbd139174fa6b6c7378241e94afa2736
Jeremy Evans <jeremy@chiefgeek.io>
2026-01-13 10:16:04 -0500

    updating

--- nvim/lua/plugins/termtoggle.lua
+++ nvim/lua/plugins/termtoggle.lua
@@ -3,7 +3,7 @@ return {
   version = "*",
   config = function()
     require("toggleterm").setup({
-      open_mapping = [[<leader>t]],
+      open_mapping = [[tt]],
       direction = "float",
       start_in_insert = true,
       close_on_exit = true,
