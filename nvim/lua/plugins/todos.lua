da36256191a242f1249732095e755291759bb595
Jeremy Evans <jeremy@chiefgeek.io>
2026-02-24 10:28:25 -0500

    working

--- nvim/lua/plugins/todos.lua
+++ nvim/lua/plugins/todos.lua
@@ -1,6 +1,28 @@
 return {
+  -- {
+  --   "leon-richardt/comment-highlights.nvim",
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   opts = {},
+  --   cmd = "CHToggle",
+  --   keys = {
+  --     {
+  --       "<leader>cc",
+  --       function()
+  --         require("comment-highlights").toggle()
+  --       end,
+  --       desc = "Toggle comment highlighting",
+  --     },
+  --   },
+  -- },
+  -- NOTE(jeremy): Wtf
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
-  }
+    highlight = {
+      pattern = [[.*\<(KEYWORDS)\%(([^)]\+)\)\?:]],
+    },
+    search = {
+      pattern = [[\b(KEYWORDS)\%(([^)]\+)\)\?:]],
+    },
+  },
 }
