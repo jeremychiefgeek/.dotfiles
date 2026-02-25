578a243a52939ce6f84e1e1b65a03c65d9b5b1da
Jeremy Evans <jeremy@chiefgeek.io>
2026-02-25 09:34:47 -0500

    updating

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -7,22 +7,30 @@ return {
     mc.setup()
 
     -- Add cursors above/below the main cursor
-    vim.keymap.set({"n", "v"}, "<up>", function() mc.lineAddCursor(-1) end)
-    vim.keymap.set({"n", "v"}, "<down>", function() mc.lineAddCursor(1) end)
+    vim.keymap.set({ "n", "v" }, "<up>", function()
+      mc.lineAddCursor(-1)
+    end)
+    vim.keymap.set({ "n", "v" }, "<down>", function()
+      mc.lineAddCursor(1)
+    end)
 
     -- Add a cursor and jump to the next word under cursor
-    vim.keymap.set({"n", "v"}, "<c-n>", function() mc.matchAddCursor(1) end)
+    vim.keymap.set({ "n", "v" }, "<c-n>", function()
+      mc.matchAddCursor(1)
+    end)
 
     -- Jump to the next word under cursor but do not add a cursor
-    vim.keymap.set({"n", "v"}, "<c-s>", function() mc.matchSkipCursor(1) end)
+    vim.keymap.set({ "n", "v" }, "<c-s>", function()
+      mc.matchSkipCursor(1)
+    end)
 
     -- Delete the main cursor
-    vim.keymap.set({"n", "v"}, "<leader>x", mc.deleteCursor)
+    vim.keymap.set({ "n", "v" }, "<leader>x", mc.deleteCursor)
 
     -- Add and remove cursors with control + left/right click
     vim.keymap.set("n", "<c-leftmouse>", mc.handleMouse)
 
-    vim.keymap.set({"n", "v"}, "<c-q>", function()
+    vim.keymap.set({ "n", "v" }, "<c-q>", function()
       if mc.cursorsEnabled() then
         mc.clearCursors()
       else
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.opt.relativenumber = true
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("let loaded_matchparen = 1")
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
 
@@ -24,7 +24,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "110"
 
 -- How I think behavior should work naturally
 vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true })
